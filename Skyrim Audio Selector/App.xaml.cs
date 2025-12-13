0e7e23128b9d2578de779fb3e2b15182777af9d6
Adam <dv23abd@cs.umu.se>
2025-12-13 05:54:11 +0100

    Add project files.

---
+++ Skyrim Audio Selector/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Skyrim_Audio_Selector
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : System.Windows.Application
+    {
+    }
+
+}
